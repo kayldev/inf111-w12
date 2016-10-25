ca0236d6e462883d798d05ca1f6625fd00316776
Aaron Lichtman <aaronlichtman@gmail.com>
2016-10-24 23:27:45 -0500

    Cleaned up code

--- src/binarySearchOnArrays.java
+++ src/binarySearchOnArrays.java
@@ -11,16 +11,14 @@ public class binarySearchOnArrays {
 	 * a target. Your task is to find the index of the target in the given
 	 * array in O(lgn). A function prototype has been provided, just fill
 	 * out the body.
-	 *
-	 *
 	 */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9},
-					x = 5,
-					y = 2,
-					z = 10;
+				x = 5,
+				y = 2,
+				z = 10;
 
 		int indexOfX = searchViaBinary(a, 0, a.length, x);
 		System.out.println("binary search finds target " + x + " at index " + indexOfX + " which should equal 4 ");
@@ -33,24 +31,26 @@ public class binarySearchOnArrays {
 
 	}
 
-
-
+	/***
+	 * @param array, lower bound, upper bound, target
+	 * @return index of target value
+	 */
 	public static int searchViaBinary(int[] arr, int lowerBound, int upperBound, int target){
+		//If the array is empty or the lowerBound is above the upperBound, return -1
 		if (arr.length == 0 || lowerBound > upperBound){
 			return -1;
 		}
-
-		/*
-		 * Work with your partner and add code here! This is a sample interview question
-		 * that Google, Facebook, etc. will ask
-		 */
-		int mid = (low + high) / 2;
-		if(arr[mid]>target)
-			return searchViaBinary(arr, lowerBound, mid-1, target);
-		else if(arr[mid] < target)
-			return searchViaBinary(arr, mid + 1, upperBound, target);
-		else
-			return mid;
+		while (lowerBound < upperBound) {
+			int mid = (lowerBound + upperBound / 2) - 1;
+			//If target is below the curr low value
+			if(arr[mid] > target)
+				return searchViaBinary(arr, lowerBound, mid-1, target);
+			//If target is above the curr low value
+			else if(arr[mid] < target)
+				return searchViaBinary(arr, mid + 1, upperBound, target);
+			//If target is curr value
+			else
+				return mid;
 		}
 	}
 }
